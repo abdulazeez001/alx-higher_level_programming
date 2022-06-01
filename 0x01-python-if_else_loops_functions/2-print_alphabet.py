719e077f2deaa29450b500858938a2f5f1b3c41d
Abdulazeez Shittu <oluwatobiabdulazeez@gmail.com>
2022-06-01 12:32:36 +0100

    release:done

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-    print("{:c}".format(character), end='')
+for a in "abcdefghijklmnopqrstuvwxyz":
+    print('{}'.format(a), end="")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for num in range(00, 100):
-    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
+for i in range(100):
+    print("{:0>2}".format(i), end=", " if i < 99 else "\n")
